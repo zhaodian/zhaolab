d7c598cec147b20633a9146ba3c2519811c337f4
zhaodian <13311885899@189.cn>
2019-03-04 08:01:09 +0800

    Create Function.py

---
+++ code/basic/Function.py
@@ -0,0 +1,18 @@
+def add(para1, para2):
+    return para1 + para2
+'''
+def：定义函数
+add：函数名
+()：里面放参数，就是调用方(谁用这个函数)告诉函数的信息
+return：返回给调用方的内容
+上面这个函数定义说成人话就是：
+我们造了一个函数，它的名字叫add，能接受2个参数para1、para2，
+函数把para1、para2的值相加后，返回给了调用方
+'''
+
+a = 1
+b = 2
+c = add(a, b)	
+# 将变量a、变量b的值赋给add函数的参数para1、para2
+# 函数add将para1、para2相加的值返回并赋值给变量c
+print(c)
